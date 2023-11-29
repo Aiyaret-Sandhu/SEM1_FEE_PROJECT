b1b312cc541c54c28cb3dfa6283371c6716780d0
= <arshsandhuprofessional@gmail.com>
2023-11-29 17:22:07 +0530

    Review page made working !

--- review/review.js
+++ review/review.js
@@ -21,3 +21,19 @@ allStar.forEach((item, idx)=> {
 		}
 	})
 })
+
+const getName = document.getElementsByClassName("addName");
+const uname = getName.value;
+const getId = document.getElementsByClassName("addId");
+const id = getId.value;
+const getReview = document.getElementsByClassName("addReview");
+const review = getReview.value;
+
+function addReview() {
+	let nameInput = document.getElementsByClassName("userName");
+	let idInput = document.getElementsByClassName("userId");
+	let reviewInput = document.getElementsByClassName("userReview");
+	nameInput.innerHTML = uname;
+	idInput.innerHTML = id;
+	reviewInput.innerHTML = review;
+}
---
+++ review/review2.js
@@ -0,0 +1,49 @@
+document.getElementById('commentForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let id = document.getElementById('id').value;
+    let comment = document.getElementById('comment').value;
+
+    let output = `
+        <h2>Submitted Comment</h2>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>ID:</strong> ${id}</p>
+        <p><strong>Comment:</strong> ${comment}</p>
+    `;
+
+    let reviewBox = `
+        <div class="testimonial-box" id="display">
+        <!--top------------------------->
+        <div class="box-top">
+            <!--profile----->
+            <div class="profile">
+                <!--img---->
+                <div class="profile-img">
+                    <img src="images/c-4.jpg" />
+                </div> 
+                <!--name-and-username-->
+                <div class="name-user">
+                    <strong class="userName">${name}</strong>
+                    <span class="userId">${id}</span>
+                </div>
+            </div>
+            <!--reviews------>
+            <div class="reviews">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i><!--Empty star-->
+            </div> 
+        </div>
+        <!--Comments---------------------------------------->
+        <div class="client-comment">
+            <p class="userReview">${comment}</p>
+        </div>
+    </div>
+    `;
+
+    document.getElementById('display').innerHTML = output;
+    document.getElementById('review').innerHTML = reviewBox;
+});
