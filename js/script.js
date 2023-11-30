1bea3f2e92fb33ab8bab5cfc93e20738b169a983
= <arshsandhuprofessional@gmail.com>
2023-11-30 23:31:56 +0530

    "Not Much"

--- js/script.js
+++ js/script.js
@@ -1,52 +1,3 @@
-var swiper = new Swiper(".product-slider", {
-    loop: true,
-    spaceBetween: 10,
-
-    autoplay: {
-        loop: true,
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    speed:3000,
- 
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1020: {
-        slidesPerView: 3,
-      },
-    },
-});
-
-var swiper = new Swiper(".review-slider", {
-    loop: true,
-    spaceBetween: 10,
-
-    autoplay: {
-        loop: true,
-        delay: 7500,
-        disableOnInteraction: false,
-    },
- 
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1020: {
-        slidesPerView: 3,
-      },
-    },
-});
-
-
-
 
 let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>
@@ -93,13 +44,14 @@ windows.onscroll = () =>
     navbar.classList.remove('active');
 };
 
-let tl = gsap.timeline({default: {duration: 0.2}});
-tl 
-    .from(".header",{opacity:0, y:10})
-    .from(".home", {opacity:0, y:10})
-    .from('.features', {opacity:0, y:8})
-    .from(".products", {opacity:0, y:10})
-    .from(".categories", {opacity:0, y:10})
-    .from(".review", {opacity:0, y:10})
-    .from(".blogs", {opacity:0, y:10})
+
+// let tl = gsap.timeline({default: {duration: 0.2}});
+// tl 
+//     .from(".header",{opacity:0, y:10})
+//     .from(".home", {opacity:0, y:10})
+//     .from('.features', {opacity:0, y:8})
+//     .from(".products", {opacity:0, y:10})
+//     .from(".categories", {opacity:0, y:10})
+//     .from(".review", {opacity:0, y:10})
+//     .from(".blogs", {opacity:0, y:10})
 
---
+++ js/second.js
@@ -0,0 +1,49 @@
+var swiper = new Swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 10,
+
+    autoplay: {
+        loop: true,
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    speed:3000,
+ 
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
+
+// var swiper = new Swiper(".review-slider", {
+//     loop: true,
+//     spaceBetween: 10,
+
+//     autoplay: {
+//         loop: true,
+//         delay: 7500,
+//         disableOnInteraction: false,
+//     },
+ 
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
+//       },
+//       768: {
+//         slidesPerView: 2,
+//       },
+//       1020: {
+//         slidesPerView: 3,
+//       },
+//     },
+// });
+
+
+
