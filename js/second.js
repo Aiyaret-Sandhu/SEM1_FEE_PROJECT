aa1a5d36c0722540aa76bfe0f362307a77333648
= <arshsandhuprofessional@gmail.com>
2023-12-01 19:22:57 +0530

    Made an infinite loop

--- js/second.js
+++ js/second.js
@@ -1,26 +1,50 @@
-var swiper = new Swiper(".product-slider", {
-    loop: true,
-    spaceBetween: 10,
+// var swiper = new Swiper(".product-slider", {
+//     loop: true,
+//     spaceBetween: 10,
 
-    autoplay: {
-        loop: true,
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    speed:3000,
+//     autoplay: {
+//         loop: true,
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
+//     speed:3000,
  
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
+//       },
+//       768: {
+//         slidesPerView: 2,
+//       },
+//       1020: {
+//         slidesPerView: 3,
+//       },
+//     },
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper = new Swiper(".product-slider", {
+      autoplay: {
+          delay: 2500,
       },
-      768: {
-        slidesPerView: 2,
+      pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
       },
-      1020: {
-        slidesPerView: 3,
-      },
-    },
+      breakpoints: {
+              0: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              1020: {
+                slidesPerView: 3,
+              },
+            }
+  });
 });
+
 
 // var swiper = new Swiper(".review-slider", {
 //     loop: true,
